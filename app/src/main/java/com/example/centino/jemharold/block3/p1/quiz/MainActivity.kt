6d1cb07190197ccf7f32902579c35244a28c08c1
Kawu <138832632+Kawudesu@users.noreply.github.com>
2023-12-05 17:29:24 +0800

    2nd Commit, BSIT2-03

--- app/src/main/java/com/example/centino/jemharold/block3/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/example/centino/jemharold/block3/p1/quiz/MainActivity.kt
@@ -5,12 +5,15 @@ import  androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.SwitchCompat
+import com.example.centino.jemharold.block3.p1.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
